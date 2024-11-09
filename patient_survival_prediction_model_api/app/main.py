3dbaf60c47f99bae584c1135ee6d3c96e294cc5d
Ramanaidu <ramanaidu.m1@gmail.com>
2024-11-09 13:23:26 +0530

    graphana changes included

--- patient_survival_prediction_model_api/app/main.py
+++ patient_survival_prediction_model_api/app/main.py
@@ -1,5 +1,12 @@
 import sys
 from pathlib import Path
+import pandas as pd
+from sklearn.metrics import r2_score
+from fastapi import Response
+import prometheus_client as prom
+from patient_survival_prediction_model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config
+from patient_survival_prediction_model.predict import make_prediction
+
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
@@ -50,6 +57,22 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
+
+test_data = dataframe = pd.read_csv(Path(f"{DATASET_DIR}/heart_failure_clinical_records_dataset.csv")) 
+r2_metric = prom.Gauge('patient_survival_score', 'R2 score for random 100 testsamples')
+# Function for updating metrics
+def update_metrics():
+    test = test_data.sample(100)
+    test_feat = test.drop('cnt', axis=1)
+    test_cnt = test['cnt'].values
+    test_pred = make_prediction(input_data=test_feat)['predictions']
+    r2 = r2_score(test_cnt, test_pred).round(3)
+    r2_metric.set(r2)
+    
+@app.get("/metrics")
+async def get_metrics():
+    update_metrics()
+    return Response(media_type="text/plain",content = prom.generate_latest())
 
 if __name__ == "__main__":
     # Use this for debugging purposes only
